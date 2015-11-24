d6663958971873526d6531d88b6b9fa6d4946a1e
Stefan <lucis0783@googleapps.wrdsb.ca>
2015-11-24 10:08:07 -0500

    hi

--- core/src/com/mygdx/game/RAID2.java
+++ core/src/com/mygdx/game/RAID2.java
@@ -3,23 +3,24 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-
-import javafx.scene.control.RadioButton;
 
 public class RAID2 extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture imgCan1, imgCan2, imgCan3, imgCan4;
 	Sprite spCan1, spCan2, spCan3, spCan4;
 	int nNum=0;
+	private Music music_level1;
 	@Override
 	public void create () {
 
+		music_level1 = Gdx.audio.newMusic(Gdx.files.internal("level1.ogg"));
+		music_level1.setLooping(true);
+		music_level1.play();
 		batch = new SpriteBatch();
 		imgCan1 = new Texture("raidant copy.png");
 		imgCan2 = new Texture("raidfly copy.png");
@@ -53,22 +54,12 @@ public class RAID2 extends ApplicationAdapter {
 								Gdx.graphics.getHeight() - 100);
 						nNum=1;
 					}
-					else{
-						spCan2.setPosition(Gdx.graphics.getWidth() - 100,
-								Gdx.graphics.getHeight() - 200);
-						nNum=0;
-					}
 				}
 				if (Gdx.input.getY() >= spCan3.getY() && Gdx.input.getY() < spCan3.getY() + spCan3.getHeight()) {
 					if(nNum==0) {
 						spCan3.setPosition(Gdx.graphics.getWidth() - 200,
 								Gdx.graphics.getHeight() - 100);
 						nNum=1;
-					}
-					else{
-						spCan3.setPosition(Gdx.graphics.getWidth() - 100,
-								Gdx.graphics.getHeight() - 300);
-						nNum=0;
 					}
 				}
 				if (Gdx.input.getY() >= spCan2.getY() && Gdx.input.getY() < spCan2.getY() + spCan2.getHeight()) {
@@ -77,21 +68,42 @@ public class RAID2 extends ApplicationAdapter {
 								Gdx.graphics.getHeight() - 100);
 						nNum=1;
 					}
-					else{
-						spCan4.setPosition(Gdx.graphics.getWidth() - 100,
-								Gdx.graphics.getHeight() - 400);
-						nNum=0;
-					}
 				}
 
 			}
 		}
+		if(Gdx.input.isButtonPressed(Input.Buttons.RIGHT)) {
+			if (Gdx.input.getX() >= spCan1.getX()) {
+				if (Gdx.input.getY() >= spCan4.getY() && Gdx.input.getY() < spCan4.getY() + spCan4.getHeight()) {
+					if (nNum == 1) {
+						spCan2.setPosition(Gdx.graphics.getWidth() - 100,
+								Gdx.graphics.getHeight() - 200);
+						nNum = 0;
+					}
+					if (Gdx.input.getY() >= spCan2.getY() && Gdx.input.getY() < spCan2.getY() + spCan2.getHeight()) {
+						if (nNum == 1) {
+							spCan4.setPosition(Gdx.graphics.getWidth() - 100,
+									Gdx.graphics.getHeight() - 200);
+							nNum = 0;
+						}
+					}
+					if (Gdx.input.getY() >= spCan3.getY() && Gdx.input.getY() < spCan3.getY() + spCan3.getHeight()) {
+						if (nNum == 1) {
+							spCan3.setPosition(Gdx.graphics.getWidth() - 100,
+									Gdx.graphics.getHeight() - 200);
+							nNum = 0;
+						}
+					}
+				}
+			}
+		}
+
 		//Problems with clicking on image locations. For unknown reasons the y-coordinates seem to be
 		//working improperly. Eg; the positions of Can2 and Can4 seem to be flipped with each others
 		//when trying to click the images, to move Can1 the bottom right corner must be pressed as
 		//opposed to the top, and Can3 works when clicked but in order to place the image back in
 		//its original location the bottom right must be clicked again.
-		// I will try my hardest to fix this ASAP
+		//I will try my hardest to fix this ASAP
 		Gdx.gl.glClearColor(1, 0, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
